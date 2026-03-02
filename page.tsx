0fe6047c22e554f74f34e185faa122fb3e6398b8
luverefounder-del <luverefounder@gmail.com>
2026-03-02 16:48:37 +0530

    chore: remove root page

--- page.tsx
+++ page.tsx
@@ -1,18 +1,1 @@
-export default function Home() {
-  return (
-    <main style={{
-      minHeight: "100vh",
-      background: "#0b0f19",
-      color: "white",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontFamily: "Arial"
-    }}>
-      <div style={{ textAlign: "center" }}>
-        <h1>ELITE PAY</h1>
-        <p>Fintech Dashboard is Live 🚀</p>
-      </div>
-    </main>
-  );
-}
+
