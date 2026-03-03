6a078250cc2489c334b9fb81f9346fbd3d21d9a0
luverefounder-del <luverefounder@gmail.com>
2026-03-03 07:07:09 +0530

    feat: add Loader component for page transitions

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,1 +1,9 @@
+"use client";
 
+export default function Loader() {
+  return (
+    <div className="fixed inset-0 bg-[#071c3b] flex items-center justify-center z-50">
+      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
